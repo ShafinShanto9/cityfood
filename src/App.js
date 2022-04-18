38f12eab077280321531669cca6c45e3b04a7e89
shafin_shant0 <shafinshanto9@gmail.com>
2022-04-18 13:07:31 +0600

    adding not found page and done header navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import CreateItem from './components/CreateItem'
 import Header from './components/Header'
+import MainContainer from './components/MainContainer'
+import NotFound from './components/NotFound'
 
 const App = () => {
   return (
     <div className='h-screen w-screen flex flex-col' >
-      <Header/>
+      <Header />
+      
+      <main className='mt-24 p-8 w-full'>
+        <Routes>
+          <Route path='/' element={<MainContainer />} />
+          <Route path='/createItem' element={<CreateItem />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </main>
+
     </div>
   )
 }

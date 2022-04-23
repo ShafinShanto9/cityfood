cf0213f0bae5c0edcdab6e815b0332d13ea47067
shafin_shant0 <shafinshanto9@gmail.com>
2022-04-23 16:27:09 +0600

    left banner complete

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
         <div className='h-screen w-screen flex flex-col' >
       <Header />
       
-      <main className=' mt-16 md:mt-24 px-4 md:px-16 py-4 w-full'>
+      <main className='w-full mt-16 md:mt-24 px-4 md:px-16 py-4'>
         <Routes>
           <Route path='/' element={<MainContainer />} />
           <Route path='/createItem' element={<CreateItem />} />
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import Delivary from '../img/delivery.png'
+import { motion } from 'framer-motion';
 
 const MainContainer = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 gap-3'>
-      <div className='py-2 flex-1 flex flex-col items-start md:items-center justify-center gap-6 '>
-          <div className='flex items-center justify-center gap-2 bg-yellow-200 mt-5 rounded-full py-1 px-3 drop-shadow-xl'>
-            <p className='text-base text-yellow-700 font-semibold'>Fast Delivary</p> 
-          <div className='w-8 h-8 rounded-full overflow-hidden'>
-              <img src={Delivary} className="w-full h-full object-contain" alt="Delivary" />
-          </div> 
-        </div>
+      <div className='py-2 flex-1 flex flex-col items-center md:items-start justify-start gap-6 '>
+        <motion.div
+          initial={{opacity:0, x:200}}
+          animate={{opacity:1, x:0}}
+          exit={{ opacity: 0, x: 200 }}
+          className='w-full flex items-center justify-center gap-2  mt-5 drop-shadow-xl'>
+          <div>
+           <p className='text-base text-yellow-700 font-semibold'>Super Cool Delivary</p></div> 
+          <div className='w-12 h-15 rounded-full overflow-hidden'>
+              <img src={Delivary} className="w-full h-full object-contain " alt="Delivary" />
+          </div>      
+        </motion.div>
         <p className='text-[2.5rem] font-bold tracking-wide text-headingColor'>We Provide You <span className='text-yellow-600 text-[3rem]'>Super Cool Delivary</span> In Your City</p>
 
         <blockquote>
@@ -18,10 +24,8 @@ const MainContainer = () => {
           <p>— Dorothy Day</p>
         </blockquote>
 
-        <button className='bg-buttonColor w-full p-4  ' >Order Now</button>
-        
-
-
+        <button type='button' className='bg-buttonColor w-full md:w-auto p-4 rounded-lg hover:shadow-xl transition-all ease-in-out duration-100 
+        ' >Order Now</button>
       </div>
       <div className='py-2 flex-1'></div>
     </div>

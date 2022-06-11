5187ada4a012d28ee19757a9672d7de63611c99a
shafin_shant0 <shafinshanto9@gmail.com>
2022-06-11 14:03:58 +0600

    fixed styling issue on filtering

--- src/components/RowContainer.js
+++ src/components/RowContainer.js
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 import React, { useEffect, useRef } from 'react';
-import { MdShoppingBag } from "react-icons/md";
+import { MdShoppingBasket } from "react-icons/md";
 
 const RowContainer = ({ flag, data, scrollValue }) => {
 
@@ -12,27 +12,63 @@ const RowContainer = ({ flag, data, scrollValue }) => {
   },[scrollValue])
 
   return (
-    <div ref={rowContainer} className={`my-12 w-full flex items-center gap-3 scroll-smooth  ${flag ? 'overflow-x-scroll scrollbar-none' : 'overflow-x-hidden flex-wrap justify-center'}`} >
-      {!data && <>
-        <Spinner name="folding-cube" color="orange" /> <p>Please Wait a moments...</p>
-      </>}
-      {data && data.map(item => (
-         <div key={item.id} className='w-300 h-[245px] min-w-[300px] md:w-340 md:min-w-[340px]  p-1 md:p-2 my-12  bg-cardOverlay rounded-lg shadow-md backdrop-blur-lg hover:drop-shadow-lg'>
-              <div className='w-full h-[150] flex items-center justify-between '>
-                  <motion.img  whileHover={{scale: 1.2}} className='w-40 drop-shadow-xl object-cover' src={item?.imageURL} alt="" />
-                  <motion.div whileTap={{scale: 0.75}} className='h-8 w-8 rounded-full bg-yellow-300 shadow-lg flex items-center justify-center cursor-pointer'>
-                    <MdShoppingBag className='text-textColor'/>
-                  </motion.div>
+     <div
+      ref={rowContainer}
+      className={`w-full flex items-center gap-3  my-12 scroll-smooth  ${
+        flag
+          ? "overflow-x-scroll scrollbar-none"
+          : "overflow-x-hidden flex-wrap justify-center"
+      }`}
+    >
+      {data && data.length > 0 ? (
+        data.map((item) => (
+          <div
+            key={item?.id}
+            className="w-275 h-[175px] min-w-[275px] md:w-300 md:min-w-[300px]  bg-cardOverlay rounded-lg py-2 px-4  my-12 backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative"
+          >
+            <div className="w-full flex items-center justify-between">
+              <motion.div
+                className="w-40 h-40 -mt-8 drop-shadow-2xl"
+                whileHover={{ scale: 1.2 }}
+              >
+                <img
+                  src={item?.imageURL}
+                  alt=""
+                  className="w-full h-full object-contain"
+                />
+              </motion.div>
+              <motion.div
+                whileTap={{ scale: 0.75 }}
+                className="w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md -mt-8"
+                // onClick={() => setItems([...cartItems, item])}
+              >
+                <MdShoppingBasket className="text-white" />
+              </motion.div>
+            </div>
+
+            <div className="w-full flex flex-col items-end justify-end -mt-8">
+              <p className="text-textColor font-semibold text-base md:text-lg">
+                {item?.title}
+              </p>
+              <p className="mt-1 text-sm text-gray-500">
+                {item?.calories} Calories
+              </p>
+              <div className="flex items-center gap-8">
+                <p className="text-lg text-headingColor font-semibold">
+                  <span className="text-sm text-red-500">$</span> {item?.price}
+                </p>
               </div>
-              <div className='w-full flex flex-col items-end justify-end'>
-            <p className='md:text-lg text-textColor font-semibold text-base'>{ item?.title}</p>
-                  <p className='mt-1 text-sm text-gray-500'>{item?.calorise} Calorise</p>
-                  <div className='flex items-center gap-8'>
-              <p className='text-lg text-headingColor font-semibold'><span className='text-sm text-yellow-800'>$</span>{ item?.price}</p>
-                  </div>
-              </div>
+            </div>
           </div>
-      ))}     
+        ))
+      ) : (
+        <div className="w-full flex flex-col items-center justify-center">
+          {/* <img src={NotFound} className="h-340" /> */}
+          <p className="text-xl text-headingColor font-semibold my-2">
+            Items Not Available
+          </p>
+        </div>
+      )}
     </div>
   )
 }

6c64848047cc123f529a5e6ea96590fd302036ad
shafin_shant0 <shafinshanto9@gmail.com>
2022-06-26 13:56:31 +0600

    completed

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion'
+import { useEffect, useState } from 'react'
 import { MdOutlineKeyboardBackspace } from "react-icons/md"
 import { RiRefreshFill } from "react-icons/ri"
 import { actionType } from '../context/reducer'
@@ -9,7 +10,9 @@ import CartItems from './CartItems'
 
 const CartContainer = () => {
 
-  const [{ cartShow, cartItems,user }, dispatch] = useStateValue()
+  const [{ cartShow, cartItems, user }, dispatch] = useStateValue()
+  const [flag, setFlag] = useState(1);
+  const [tot, setTot] = useState(0);
   
   const showCart = () => {
     dispatch({
@@ -17,6 +20,23 @@ const CartContainer = () => {
       cartShow : !cartShow
     })
   }
+
+   useEffect(() => {
+    let totalPrice = cartItems.reduce(function (accumulator, item) {
+      return accumulator + item.qty * item.price;
+    }, 0);
+    setTot(totalPrice);
+    console.log(tot);
+  }, [tot, flag]);
+
+  const clearCart = () => {
+    dispatch({
+      type: actionType.SET_CART_ITEMS,
+      cartItems: [],
+    });
+
+    localStorage.setItem("cartItems", JSON.stringify([]));
+  };
   return (
     <motion.div
       initial={{ opacity: 0, x: 200 }}
@@ -30,7 +50,7 @@ const CartContainer = () => {
           <MdOutlineKeyboardBackspace className="text-textColor text-3xl" />
         </motion.div>
         <p className='text-textColor text-lg font-semibold'>Cart Menu</p>
-        <motion.p whileTap={{scale: 0.75}} className='flex items-center gap-2 p-1 px-2 my-2 bg-red-400 rounded-md hover:shadow-md cursor-pointer text-white text-base'>Clear Cart Items <RiRefreshFill/> </motion.p>
+        <motion.p whileTap={{scale: 0.75}} className='flex items-center gap-2 p-1 px-2 my-2 bg-red-400 rounded-md hover:shadow-md cursor-pointer text-white text-base' onClick={clearCart}>Clear Cart Items <RiRefreshFill/> </motion.p>
       </div>
     {/*  Cart Items Section */}
       <div className='w-full h-full bg-gray-100 rounded-t-[2rem] flex flex-col'>
@@ -38,8 +58,12 @@ const CartContainer = () => {
           
           {/*  Show Single Cart Items */}
           {
-            cartItems?.map((cart) => (              
-                <CartItems cart={cart} />
+            cartItems?.map((item) => (              
+              <CartItems
+                key={item.id}
+                item={item}
+                setFlag={setFlag}
+                flag={flag} />
             ))
           }
         </div>
@@ -49,16 +73,16 @@ const CartContainer = () => {
           <div className='w-full flex-1 bg-gray-200 rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2'>
           <div className='w-full flex items-center justify-between'>
             <p className='text-black text-lg'>Sub Total</p>
-            <p className='text-black text-lg'>$ 8.05</p>
+              <p className='text-black text-lg'>$ {tot}</p>
           </div>
           <div className='w-full flex items-center justify-between'>
             <p className='text-black text-lg'>Delivary</p>
-            <p className='text-black text-lg'>$ 5.05</p>
+              <p className='text-black text-lg'>$ 3.0</p>
           </div>
           <div className='w-full border-b border-gray-700 my-2'></div>
           <div className='w-full flex items-center justify-between'>
             <p className='text-black text-xl font-semibold'>Total</p>
-            <p className='text-black text-xl font-semibold'>$ 13.10</p>
+              <p className='text-black text-xl font-semibold'>$ { tot + 3.0}</p>
           </div>
           
             {
--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,42 +1,74 @@
-import { motion } from 'framer-motion'
-import { useState } from 'react'
-import { BiMinus, BiPlus } from 'react-icons/bi'
-import { useStateValue } from '../context/StateProvider'
-const CartItems = ({ cart }) => {
-    const [qty, setQty] = useState(1)
-     const [{  cartItems }, dispatch] = useStateValue()
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { BiMinus, BiPlus } from 'react-icons/bi';
+import { actionType } from '../context/reducer';
+import { useStateValue } from '../context/StateProvider';
+let items = [];
+
+const CartItems = ({ item, setFlag, flag }) => {
+    
+    const [{ cartItems }, dispatch] = useStateValue();
+    const [qty, setQty] = useState(item?.qty);
+
+    const cartDispatch = () => {
+        localStorage.setItem("cartItems", JSON.stringify(items));
+        dispatch({
+        type: actionType.SET_CART_ITEMS,
+        cartItems: items,
+    });
+    };
 
     const updateQty = (action, id) => {
-        if (action == 'add') {
-            setQty(qty + 1)
-
-            cartItems.map(items => {
-                if (items.id == id) {
-                    items.qty += 1;
-                }
-            })
-
+        if (action == "add") {
+        setQty(qty + 1);
+        cartItems.map((item) => {
+            if (item.id === id) {
+                item.qty += 1;
+                setFlag(flag + 1);
+            }
+    });
+    cartDispatch();
+    } else {
+    // initial state value is one so you need to check if 1 then remove it
+     if (qty == 1) {
+        items = cartItems.filter((item) => item.id !== id);
+        setFlag(flag + 1);
+        cartDispatch();
+    } else {
+        setQty(qty - 1);
+        cartItems.map((item) => {
+        if (item.id === id) {
+            item.qty -= 1;
+            setFlag(flag + 1);
         }
+    });
+        cartDispatch();
     }
+    }
+    };
+
+    useEffect(() => {
+        items = cartItems;
+    }, [qty, items]);
 
   return (
-    <div key={cart.id} className='w-full p-1 px-2 rounded-lg bg-cartBg flex items-center gap-2'>
-                <img src={cart.imageURL}
+    <div  className='w-full p-1 px-2 rounded-lg bg-cartBg flex items-center gap-2'>
+                <img src={item.imageURL}
                   className='h-20 w-20 max-w-[60px] rounded-full object-contain' alt="" srcset="" />
                 
                 <div className='flex flex-col gap-2'>
-                  <p className='text-base text-black'> {cart?.title} </p>
-                  <p className='text-sm block text-black text-semibold'> $ {parseFloat(cart?.price) * qty} </p>
+                  <p className='text-base text-black'> {item?.title} </p>
+                  <p className='text-sm block text-black text-semibold'> $ {parseFloat(item?.price) * qty} </p>
                 </div>
 
                 <div className='group flex items-center gap-2 ml-auto cursor-pointer'> 
-                  <motion.div whileTap={{scale: 0.6}} onClick={()=>updateQty("remove", cart?.id)}>
+                  <motion.div whileTap={{scale: 0.6}} onClick={()=>updateQty("remove", item?.id)}>
                     <BiMinus className='text-black'/>
                   </motion.div>
                   <p className='w-5 h-5 rounded-sm bg-cartBg text-black flex items-center justify-center '>
                     {qty}
                   </p>
-                  <motion.div whileTap={{scale: 0.6}} onClick={()=>updateQty("add", cart?.id)}>
+                  <motion.div whileTap={{scale: 0.6}} onClick={()=>updateQty("add", item?.id)}>
                     <BiPlus className='text-black'/>
                   </motion.div>
                 </div>
